ac2a0dd510e14386cc12272382a91be14900ffee
Andrius <andriusjavait@gmail.com>
2017-01-15 13:01:17 +0000

    servlet class update doGet method with servlet getInitParameter() with explanation of using it

--- Chapter5_1/src/com/example/servlet/Chapter5Servlet.java
+++ Chapter5_1/src/com/example/servlet/Chapter5Servlet.java
@@ -12,6 +12,14 @@ public class Chapter5Servlet extends HttpServlet {
 	   out.println("<html>");
        out.println("<head><title>Test ServletConfig()</title></head>");
        out.println("<body><h3>getServletConfig().getInitParameter(\"adminEmail\"):</h3><br>");
+       // when you are become Servlet object remeber servlet life cycle with constructor and init method
+       // so This getServletConfig() method come from Servlet interface, so using this you can
+       // get servlet config object, and using that object you can get init parameter which are 
+       // always set in DD inside <servlet> when we talking about servlet init parameter, no where
+       // else can be set. method getInitParamter(String): String :
+       out.println("<i>" + this.getServletConfig().getInitParameter("adminEmail") + "</i><hr>");
+       out.println("<body><h3>getServletConfig().getInitParameter(\"otherEmail\"):</h3><br>");
+       out.println("<i>" + this.getServletConfig().getInitParameter("otherEmail") + "</i><hr>");
        out.println("</body></html>");
   }
   public void doPost(HttpServletRequest request, 
