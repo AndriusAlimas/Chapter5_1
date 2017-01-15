fe491102fa165f0f3cedce46e0cf34008af992fb
Andrius <andriusjavait@gmail.com>
2017-01-15 12:20:30 +0000

    update servlet class some printing done and created empty doPost()

--- Chapter5_1/src/com/example/servlet/Chapter5Servlet.java
+++ Chapter5_1/src/com/example/servlet/Chapter5Servlet.java
@@ -6,6 +6,16 @@ import java.io.*;
 public class Chapter5Servlet extends HttpServlet {
   public void doGet(HttpServletRequest request, 
 		  HttpServletResponse response)throws ServletException,IOException{
+	   response.setContentType("text/html");
+	   PrintWriter out = response.getWriter();
+	   
+	   out.println("<html>");
+       out.println("<head><title>Test ServletConfig()</title></head>");
+       out.println("<body><h3>getServletConfig().getInitParameter(\"adminEmail\"):</h3><br>");
+       out.println("</body></html>");
+  }
+  public void doPost(HttpServletRequest request, 
+		  HttpServletResponse response)throws ServletException,IOException{
 	  
   }
 }
